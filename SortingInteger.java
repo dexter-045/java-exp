4d8900b9717d30ac4d676747a071439b6d4e3a6c
dexter-045 <dexteracc45@gmail.com>
2024-11-04 22:23:47 +0530

    Update SortingInteger.java

--- SortingInteger.java
+++ SortingInteger.java
@@ -13,7 +13,7 @@ class SortingDemo {
 	}
 
     for (i = 0; i < (n-1); i++) {
-    	for (j=0; j< (n -1 -1); j++) {
+    	for (j=0; j< (n -i -1); j++) {
             if (array[j] > array[j+1]) { 
 
                temp = array[j];
